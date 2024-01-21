ff1a176ba44dc0cc6e2dbdf68a5e776f582bd0ae
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-01-21 19:18:07 +0800

    update

--- src/Base/component/Navbar.jsx
+++ src/Base/component/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900 mt-5">
+        <nav className="hidden lg:inline bg-white border-gray-200 dark:bg-gray-900 mt-5">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src={logo} className="h-10" alt="Logo" />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../Base/component/Navbar";
 import Hero from "../Base/component/Hero";
+import BottomNavbar from "../Base/component/BottomNavbar";
 
 const Home = () => {
+
     return (
         <>
             <div className="container mx-auto">
                 <Navbar></Navbar>
+                <BottomNavbar></BottomNavbar>
             </div>
             <Hero></Hero>
         </>
