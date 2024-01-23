1eaf0a2a6333df552cb0283498790dbcee9ff50d
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-01-23 17:26:31 +0800

    delete class container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from "../Base/component/Hero";
 import BottomNavbar from "../Base/component/BottomNavbar";
 import SearchHero from "../Base/component/SearchHero";
 import About from "../Base/component/About";
+import Statistic from "../Base/component/Statistic";
 
 const Home = () => {
 
@@ -14,6 +15,7 @@ const Home = () => {
             <Hero></Hero>
             <SearchHero></SearchHero>
             <About></About>
+            <Statistic></Statistic>
         </>
     )
 }
