c9d886932f8c269bccd88d048cf1e1cad25fd128
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-01-23 17:25:58 +0800

    update navbar desktop

--- src/Base/component/Navbar.jsx
+++ src/Base/component/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="hidden lg:inline bg-white border-gray-200 dark:bg-gray-900 mt-5 sticky top-0">
+        <nav className="hidden lg:inline bg-white border-gray-200 dark:bg-gray-900 mt-5 sticky top-0 z-50">
             <div className="max-w-screen bg-white">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
